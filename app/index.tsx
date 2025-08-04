41b66765388c202a4616a6371dca8bd292e49015
Hiroki Chiyoda <contact@arafipro.com>
2025-08-04 13:47:30 +0900

    コンポーネントのリファクタリング
    HomeコンポーネントからDummyJsonDataコンポーネントに置換

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,5 @@
-import * as schema from "@/db/schema";
-import { Task } from "@/db/schema";
-import { drizzle } from "drizzle-orm/expo-sqlite";
-import { useSQLiteContext } from "expo-sqlite";
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import DummyJsonData from "@/components/dummy-json-data";
 
-export default function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-
-  const db = useSQLiteContext();
-  const drizzleDb = drizzle(db, { schema });
-
-  useEffect(() => {
-    const load = async () => {
-      const tasks = await drizzleDb.query.tasks.findMany();
-      setTasks(tasks);
-    };
-    load();
-  }, []);
-
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text>{JSON.stringify(tasks, null, 2)}</Text>
-    </View>
-  );
+export default function Index() {
+  return <DummyJsonData />;
 }
